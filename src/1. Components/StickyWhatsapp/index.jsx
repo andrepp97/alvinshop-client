841ded516e50d4572382f49177bb1ad7efc951a0
andrepp97 <andreputerap@gmail.com>
2020-08-18 05:35:50 +0700

    Sticky Whatsapp

---
+++ src/1. Components/StickyWhatsapp/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { MDBIcon } from 'mdbreact';
+import './style.css';
+
+const StickyWhatsapp = () => {
+    return (
+        <div className="sticky-whatsapp">
+            <MDBIcon fab icon="whatsapp" />
+        </div>
+    );
+};
+
+export default StickyWhatsapp;

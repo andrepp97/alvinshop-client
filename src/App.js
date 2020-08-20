53131e01c688cd11b48599ff97341f8c505a6a5a
andrepp97 <andreputerap@gmail.com>
2020-08-21 00:14:20 +0700

    add Footer component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import StickyWhatsapp from './1. Components/StickyWhatsapp';
 // PAGES
 import Routes from './Routes';
 import Navbar from './1. Components/Navbar';
+import Footer from './1. Components/Footer';
 
 class App extends Component {
   render() {
@@ -12,6 +13,7 @@ class App extends Component {
         <Navbar />
         <Routes />
         <StickyWhatsapp />
+        <Footer />
       </div>
     );
   }

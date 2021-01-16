f3b932013ef51bf0065d973a88bbaa6fcc8f2ba5
Andre Putera Pratama <andreputerap@gmail.com>
2021-01-16 22:59:57 +0700

    Update Footer
    integrate with settings from API

--- src/1. Components/Footer/index.jsx
+++ src/1. Components/Footer/index.jsx
@@ -5,116 +5,75 @@ import {
     MDBRow,
     MDBCol,
 } from "mdbreact";
-import './footer.css';
 
 import bl from '../../3. Assets/img/marketplace/bukalapak.png';
 import shp from '../../3. Assets/img/marketplace/shopee.png';
 import tkp from '../../3. Assets/img/marketplace/tokopedia.png';
 
-const links = [
-    {
-        text: "Link 1",
-        url: "https://www.youtube.com/"
-    },
-    {
-        text: "Link 2",
-        url: "https://www.youtube.com/"
-    },
-    {
-        text: "Link 3",
-        url: "https://www.youtube.com/"
-    },
-]
-
 const marketplace = [
     {
-        text: "Bukalapak",
+        text: "bukalapak",
         image: bl,
-        url: "https://www.bukalapak.com/"
+        url: "https://www.bukalapak.com/u/"
     },
     {
-        text: "Shopee",
+        text: "shopee",
         image: shp,
         url: "https://shopee.co.id/"
     },
     {
-        text: "Tokopedia",
+        text: "tokopedia",
         image: tkp,
         url: "https://www.tokopedia.com/"
     },
 ]
 
-const FooterSection = () => {
+const FooterSection = ({settings}) => {
   return (
     <MDBFooter color="special-color-dark" className="font-small py-5">
       <MDBContainer className="text-center text-md-left">
 
         <MDBRow>
           <MDBCol md="4" className="mb-5">
-            <h5 className="text-uppercase">Alvinshop</h5>
+            <h5 className="text-uppercase">
+                {settings.name}
+            </h5>
             <small className="opacity-70">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem autem soluta architecto rem facere nisi unde recusandae placeat ab ad reprehenderit quasi, laboriosam voluptatum.
+              {settings.description}
             </small>
           </MDBCol>
           <MDBCol md="4" className="mb-5">
-            <h5 className="text-uppercase">Useful Links</h5>
-            <div className="row">
-                <div className="col">
-                    <ul className="list-group">
-                        {links.map((link,idx) => (
-                            <li key={idx} className="useful-links border-bottom">
-                                <a href={link.url}>
-                                    {link.text}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-                <div className="col">
-                    <ul className="list-group">
-                        {links.map((link,idx) => (
-                            <li key={idx} className="useful-links border-bottom">
-                                <a href={link.url}>
-                                    {link.text}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-          </MDBCol>
-          <MDBCol md="4" className="mb-5">
-            <h5 className="text-uppercase">Offline Store</h5>
-            <div className="opacity-70">
-                <span>Mall Baru Buka</span>
-                <br/>
-                <span>Jl. Buntu 69</span>
-                <br/>
-                <span>Jakarta</span>
-            </div>
-          </MDBCol>
-        </MDBRow>
-
-        <div className="text-center">
-            <h5 className="text-uppercase mb-3">
-                Marketplace
-            </h5>
+            <h5 className="text-uppercase">Marketplace</h5>
             {marketplace.map(item => (
-                <a key={item.text} href={item.url} target="_blank" rel="noopener noreferrer">
+                <a
+                    key={item.text}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href={item.url + settings[item.text]}
+                >
                     <img
-                        className="rounded mx-2"
+                        className="rounded m-2"
                         src={item.image} 
                         alt={item.text}
                         height={60}
                     />
                 </a>
             ))}
-        </div>
+          </MDBCol>
+          <MDBCol md="4" className="mb-5">
+            <h5 className="text-uppercase">Offline Store</h5>
+            <div className="opacity-70">
+                <span>
+                    {settings.address}
+                </span>
+            </div>
+          </MDBCol>
+        </MDBRow>
 
         <hr className="border-white my-5" />
 
         <div className="text-center spacing-1">
-            &copy; {new Date().getFullYear()} ALVINSHOP - All Rights Reserved.
+            &copy; {new Date().getFullYear()} <span className="text-uppercase">{settings.name}</span> - All Rights Reserved.
         </div>
 
       </MDBContainer>

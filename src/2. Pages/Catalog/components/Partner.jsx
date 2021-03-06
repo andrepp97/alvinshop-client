e1251a31de4e68457a25e4092b7e5fef820a94c7
Andre Putera Pratama <andreputerap@gmail.com>
2021-03-06 19:35:15 +0700

    Update Partner.jsx

--- src/2. Pages/Catalog/components/Partner.jsx
+++ src/2. Pages/Catalog/components/Partner.jsx
@@ -1,87 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import { MDBAnimation } from 'mdbreact';
 import Carousel from 'react-multi-carousel';
 
-import Thunder from '../../../3. Assets/img/partner/thunderx3-logo.png';
+import Loader from '../../../1. Components/Loader';
+import APIRequest from '../../../4. Api/APIRequest';
+import { BASE_URL } from '../../../5. Helper/settings';
 
 const responsive = {
-    xxl: {
-        breakpoint: { max: 4000, min: 1920 },
-        items: 6
-    },
-    xl: {
-        breakpoint: { max: 1920, min: 1600 },
-        items: 5
-    },
     lg: {
-        breakpoint: { max: 1600, min: 1320 },
-        items: 4
-    },
-    md: {
-        breakpoint: { max: 1320, min: 1025 },
+        breakpoint: { max: 1600, min: 660 },
         items: 3
     },
+    md: {
+        breakpoint: { max: 660, min: 550 },
+        items: 2.5
+    },
     sm: {
-        breakpoint: { max: 1025, min: 991 },
-        items: 2.5
-    },
-    sm2: {
-        breakpoint: { max: 991, min: 850 },
-        items: 3.25
-    },
-    xs: {
-        breakpoint: { max: 850, min: 630 },
-        items: 2.5
-    },
-    mobile: {
-        breakpoint: { max: 630, min: 520 },
+        breakpoint: { max: 550, min: 0 },
         items: 2
     },
-    mobile2: {
-        breakpoint: { max: 520, min: 400 },
-        items: 1.4
-    },
-    mobile3: {
-        breakpoint: { max: 400, min: 0 },
+    xs: {
+        breakpoint: { max: 460, min: 0 },
         items: 1
-    }
+    },
 }
 
-const data = [
-    {
-        id: 1,
-        name: "Thunder X3",
-        image: Thunder
-    },
-    {
-        id: 2,
-        name: "Razer",
-        image: Thunder
-    },
-    {
-        id: 3,
-        name: "Samsung",
-        image: Thunder
-    },
-    {
-        id: 4,
-        name: "Logitech",
-        image: Thunder
-    },
-    {
-        id: 5,
-        name: "Microsoft",
-        image: Thunder
-    },
-    {
-        id: 6,
-        name: "Microsoft",
-        image: Thunder
-    },
-]
-
 const PartnerCarousel = () => {
-    return (
+    // STATE
+    const prefix = "uploads/our_partner"
+    const [partners, setPartners] = useState(null)
+    const [loading, setLoading] = useState(true)
+
+    // GET DATA
+    const getPartners = useCallback(() => {
+        APIRequest.get('user/ourPartner')
+        .then(({data}) => {
+            // console.log('Hasil', data)
+            setPartners(data.data)
+        })
+        .catch(err => console.log(err))
+        .finally(() => setLoading(false))
+    }, [])
+
+    // LIFECYCLE
+    useEffect(() => {
+        getPartners()
+    }, [getPartners])
+
+    // RENDER
+    return loading
+    ? <Loader />
+    : (
         <MDBAnimation reveal type="fadeInUp">
             <Carousel
                 showDots={true}
@@ -89,15 +58,21 @@ const PartnerCarousel = () => {
                 responsive={responsive}
                 containerClass="mb-5 pb-4 pt-2"
             >
-                {data.map(item => (
-                    <img
-                        height={60}
+                {partners ? partners.map(item => (
+                    <a
                         key={item.id}
-                        alt={item.name}
-                        src={item.image}
-                        className="border rounded img-partner"
-                    />
-                ))}
+                        href={item.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="img-partner p-2"
+                    >
+                        <img
+                            className="img-fluid"
+                            alt={item.partner_name}
+                            src={BASE_URL + prefix + '/' + item.logo}
+                        />
+                    </a>
+                )) : null}
             </Carousel>
         </MDBAnimation>
     );

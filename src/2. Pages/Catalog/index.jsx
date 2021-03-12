27800c5afcb9690c9b48af80b77fc419d9947415
Andre Putera Pratama <andreputerap@gmail.com>
2021-03-12 23:37:02 +0700

    Update Catalog page
    integrate with API

--- src/2. Pages/Catalog/index.jsx
+++ src/2. Pages/Catalog/index.jsx
@@ -1,52 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
-import { MDBAnimation } from 'mdbreact';
+import {
+    MDBBtn,
+    MDBAnimation,
+    MDBListGroup,
+    MDBListGroupItem,
+} from 'mdbreact';
 import './catalog.css';
 
 // COMPONENTS
-import MasonryCarousel from './components/MasonryCarousel';
 import CarouselFull from './components/CarouselFull';
 import Carousel from './components/Carousel';
 import Partner from './components/Partner';
 import Loader from '../../1. Components/Loader';
+import ProductCard from '../../1. Components/ProductCard';
 import APIRequest from '../../4. Api/APIRequest';
-
-const discover = [
-    {
-        key: "node1",
-        icon: "gamepad",
-        label: "Playstation",
-        nodes: [
-            { key: "node1-1", label: "PS4" },
-            { key: "node1-2", label: "PS5" },
-        ]
-    },
-    {
-        key: "node2",
-        icon: "laptop",
-        label: "PC",
-        nodes: [
-            { key: "node2-1", label: "Headset" },
-            { key: "node2-2", label: "Keyboard" },
-            { key: "node2-3", label: "Mouse" },
-        ]
-    },
-]
 
 const Catalog = () => {
     // PARAM
     const {key} = useParams()
 
     // STATE
-    const [selectedData, setSelectedData] = useState('')
     const [recommended, setRecommended] = useState(null)
     const [offer, setOffer] = useState(null)
+    const [categories, setCategories] = useState([])
+    const [genres, setGenres] = useState([])
+    const [selectedCategory, setSelectedCategory] = useState('')
+    const [selectedGenre, setSelectedGenre] = useState('')
+    const [products, setProducts] = useState(null)
 
-    // CRUD
+    // GET DATA
+    const getFilters = useCallback(async () => {
+        try {
+            const query = { device_id: key }
+            const res = await APIRequest.get('user/getListFilter', { params: query })
+            const {data} = res.data
+            setCategories(data.category)
+            setGenres(data.genre)
+        } catch (err) {
+            console.log('Error Filters', err)
+        }
+    }, [key])
+
+    const getProductByFilters = useCallback(async () => {
+        try {
+            const body = {
+                genre_id: selectedGenre,
+                category_id: selectedCategory,
+            }
+            const res = await APIRequest.post('user/getProductByGenreAndCategory', body)
+            const {data} = res.data
+            setProducts(data)
+        } catch (err) {
+            console.log('Error Product by Filters', err)
+        }
+    }, [selectedGenre, selectedCategory])
+
     const getRecommended = async () => {
         try {
             const res = await APIRequest.get('user/recommendProduct')
-            console.log('Recommended', res.data.data)
+            // console.log('Recommended', res.data.data)
             const {data} = res.data
             setRecommended(data)
         } catch (err) {
@@ -57,7 +70,7 @@ const Catalog = () => {
     const getOffer = async () => {
         try {
             const res = await APIRequest.get('user/todayOffer')
-            console.log('Todays Offer', res.data.data)
+            // console.log('Todays Offer', res.data.data)
             const {data} = res.data
             setOffer(data)
         } catch (err) {
@@ -67,10 +80,11 @@ const Catalog = () => {
 
     // LIFECYCLE
     useEffect(() => {
-        setSelectedData(key)
         getRecommended()
         getOffer()
-    }, [key])
+        getFilters()
+        getProductByFilters()
+    }, [getFilters, getProductByFilters])
 
     // RENDER
     return (
@@ -80,62 +94,85 @@ const Catalog = () => {
 
             <div className="container-fluid pb-5 mt-5 px-4 px-md-5">
 
-                <MasonryCarousel />
-
                 <div className="row mt-5">
 
                     {/* LEFT COLUMN */}
                     <div className="col-lg-3 mb-4 mb-lg-0">
                         <div className="sticky-top pt-4 pt-lg-5 mt-n3">
-                            <div className="card">
-                                <div className="card-body">
-                                    <p className="font-weight-bold spacing-1 opacity-80">
-                                        DISCOVER
-                                    </p>
-                                    <div className="list-group">
-                                        {discover.map(item => (
-                                            <div
-                                                key={item.key}
-                                                className="list-group-item"
+                            <div>
+                                <p className="font-weight-bold spacing-1 opacity-80">
+                                    CATEGORY
+                                </p>
+                                <hr/>
+                                <div className="list-group">
+                                    <MDBListGroup>
+                                        {categories.map(item => (
+                                            <MDBListGroupItem
+                                                hover
+                                                key={item.id}
+                                                className="pointer-cursor border-0"
+                                                active={selectedCategory === item.id}
+                                                onClick={() => setSelectedCategory(item.id)}
                                             >
-                                                <span className="font-weight-bold">
-                                                    {item.label}
-                                                </span>
-                                                <div>
-                                                    {item.nodes.map(node => (
-                                                        <div
-                                                            key={node.key}
-                                                            onClick={() => this.setState({ selectedData: node.label })}
-                                                            className={`node-item pl-3 py-1 ${selectedData === node.label ? "node-item-active" : ""}`}
-                                                        >
-                                                            {node.label}
-                                                        </div>
-                                                    ))}
-                                                </div>
-                                            </div>
+                                                {item.name}
+                                            </MDBListGroupItem>
                                         ))}
-                                    </div>
+                                    </MDBListGroup>
                                 </div>
                             </div>
                         </div>
                     </div>
 
                     {/* RIGHT COLUMN */}
-                    <div className="col-lg-9">
+                    <div className="col-lg-9 pt-4">
 
-                        {/* MAIN CAROUSEL */}
                         <span className="text-uppercase font-weight-bolder spacing-2">
                             Recommended
                         </span>
                         {recommended ? <Carousel data={recommended} /> : <Loader />}
 
-                        {/* SECONDARY CAROUSEL */}
                         <span className="text-uppercase font-weight-bolder spacing-2">
                             Today's Offer
                         </span>
                         {offer ? <Carousel data={offer} /> : <Loader />}
 
-                        {/* PARTNERS CAROUSEL */}
+
+                        <div className="mb-5 pb-4">
+                            <div
+                                id="genre-section"
+                                className="border-top border-bottom mb-3"
+                            >
+                                {genres.map(item => (
+                                    <MDBBtn
+                                        key={item.id}
+                                        id="btn-genre"
+                                        color="indigo"
+                                        className="rounded-pill"
+                                        outline={selectedGenre !== item.id}
+                                        onClick={() => setSelectedGenre(item.id)}
+                                    >
+                                        {item.name}
+                                    </MDBBtn>
+                                ))}
+                            </div>
+                            <div className='row'>
+                                {
+                                    products
+                                    ? products.length
+                                        ? products.map(product => (
+                                            <div
+                                                key={product.product_id}
+                                                className="col-sm-6 col-md-4 col-xl-3"
+                                            >
+                                                <ProductCard item={product} />
+                                            </div>
+                                        ))
+                                        : <p>No Result</p>
+                                    : <Loader />
+                                }
+                            </div>
+                        </div>
+
                         <span className="text-uppercase font-weight-bolder spacing-2">
                             Our Partners
                         </span>

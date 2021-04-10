624c6ff9c8e921aa972ec9fd251f8b4aea1b8008
Andre Putera Pratama <andreputerap@gmail.com>
2021-04-11 04:07:25 +0700

    Update SearchModal.jsx

--- src/1. Components/Navbar/components/SearchModal.jsx
+++ src/1. Components/Navbar/components/SearchModal.jsx
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 import { MDBModal, MDBModalBody, MDBInput } from 'mdbreact';
+import { BASE_URL } from '../../../5. Helper/settings';
+import APIRequest from '../../../4. Api/APIRequest';
+import Loader from '../../Loader';
 
 const SearchModal = ({ isOpen, toggleSearch }) => {
+    // REF
+    const didMount = useRef(false)
+
+    // STATE
+    const [text, setText] = useState('')
+    const [result, setResult] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    // SEARCH FUNCTION
+    const onSearch = useCallback(async () => {
+        try {
+            setLoading(true)
+
+            const res = await APIRequest.get('user/searchProduct', {
+                params: {
+                    keyword: text
+                }
+            })
+            const { data } = res
+            setResult(data.data)
+
+            console.log('Hasil Search', data)
+        } catch (err) {
+            console.log(err.response)
+        } finally {
+            setLoading(false)
+        }
+    }, [text])
+
+    // LIFECYCLE
+    useEffect(() => {
+        if (didMount) {
+            const debounceFunction = setTimeout(() => {
+                onSearch()
+            }, 1000)
+
+            return () => clearTimeout(debounceFunction)
+        } else {
+            didMount.current = true
+        }
+    }, [onSearch])
+
+    // RENDER
     return (
-        <MDBModal isOpen={isOpen} toggle={toggleSearch} centered>
+        <MDBModal isOpen={isOpen} toggle={toggleSearch}>
 
             <div style={{ height: '25px' }}>
                 <span id="close-btn" onClick={toggleSearch}>
@@ -13,22 +59,42 @@ const SearchModal = ({ isOpen, toggleSearch }) => {
 
             <MDBModalBody className="pt-0 px-4 my-3">
 
-                <h4 className="h4-responsive text-center">SEARCH</h4>
-                <MDBInput outline label="Search the store" icon="search" />
+                <h4 className="h4-responsive text-center">
+                    SEARCH
+                </h4>
 
-                <div className="list-group">
-                    <div className="list-group-item" style={{ cursor:'pointer' }}>
-                        <i className="fa fa-gamepad mr-3" />
-                        Hasil Search 1
-                    </div>
-                    <div className="list-group-item" style={{ cursor:'pointer' }}>
-                        <i className="fa fa-gamepad mr-3" />
-                        Hasil Search 2
-                    </div>
-                    <div className="list-group-item" style={{ cursor:'pointer' }}>
-                        <i className="fa fa-gamepad mr-3" />
-                        Hasil Search 3
-                    </div>
+                <MDBInput
+                    outline
+                    icon="search"
+                    label="Search the store"
+                    onChange={e => setText(e.target.value)}
+                    value={text}
+                />
+
+                <div className="row">
+                    {
+                        loading
+                            ? <Loader />
+                            : result && result.length
+                                ? result.map(item => (
+                                    <div id={item.product_id} className="col-6">
+                                        <a
+                                            className="invisible-card"
+                                            href={`/product/${item.product_id}`}
+                                        >
+                                            <img
+                                                alt={item.name}
+                                                className="img-fluid rounded"
+                                                src={BASE_URL + item.product_image}
+                                            />
+                                            <span className="product-title">
+                                                {item.title}
+                                            </span>
+                                        </a>
+                                    </div>
+                                ))
+                                : null
+                    }
                 </div>
 
             </MDBModalBody>

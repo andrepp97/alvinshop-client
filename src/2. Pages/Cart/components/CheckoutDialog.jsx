0732b9221006f06f25a8e7bac28be103e0f4e387
Andre Putera Pratama <andreputerap@gmail.com>
2021-04-11 05:04:54 +0700

    update Checkout

--- src/2. Pages/Cart/components/CheckoutDialog.jsx
+++ src/2. Pages/Cart/components/CheckoutDialog.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Swal from 'sweetalert2';
 import {
+    MDBBtn,
     MDBModal,
     MDBModalBody,
 } from 'mdbreact';
+import APIRequest from '../../../4. Api/APIRequest';
 
 const rekening = [
     {
@@ -21,6 +24,40 @@ const CheckoutDialog = (props) => {
     // PROPS
     const { isOpen, toggleCheckout } = props
 
+    // STATE
+    const [selectedBank, setSelectedBank] = useState('')
+    const [transferDate, setTransferDate] = useState('')
+    const [image, setImage] = useState(null)
+    const [loading, setLoading] = useState(false)
+
+    // CHECKOUT
+    const userCheckout = () => {
+        try {
+            setLoading(true)
+
+            let formData = new FormData
+            formData.append('image', image)
+            formData.append('bank', selectedBank.bank)
+            formData.append('nama_rekening', selectedBank.name)
+            formData.append('tgl_transfer', transferDate)
+
+            APIRequest.post('user/order', formData)
+                .then(() => {
+                    toggleCheckout()
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Berhasil Checkout',
+                        didClose: () => window.location.reload(),
+                    })
+                })
+                .catch(err => console.log('Error Checkout', err.response))
+                .finally(() => setLoading(false))
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
     // RENDER
     return (
         <MDBModal isOpen={isOpen} toggle={toggleCheckout} centered>
@@ -36,15 +73,63 @@ const CheckoutDialog = (props) => {
                     Checkout
                 </h4>
 
-                <hr/>
+                <hr />
 
-                <select className="custom-select">
-                    {rekening.map((item, idx) => (
-                        <option key={idx} value={item}>
-                            {item.bank}
+                <div className="my-3">
+                    <span>Bank Account</span>
+                    <select
+                        defaultValue="0"
+                        className="custom-select"
+                        onChange={e => setSelectedBank(JSON.parse(e.target.value))}
+                    >
+                        <option
+                            disabled
+                            value="0"
+                        >
+                            -- Select Bank Account --
                         </option>
-                    ))}
-                </select>
+                        {rekening.map((item, idx) => (
+                            <option key={idx} value={JSON.stringify(item)}>
+                                {item.bank}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+
+                <div className="my-3">
+                    <span>Transfer Date</span>
+                    <input
+                        type="date"
+                        className="form-control"
+                        onChange={e => setTransferDate(e.target.value)}
+                        value={transferDate}
+                    />
+                </div>
+
+                <div className="my-3">
+                    <span>Upload</span>
+                    <input
+                        type="file"
+                        accept="image/*"
+                        className="form-control"
+                        onChange={e => setImage(e.target.files[0])}
+                    />
+                </div>
+
+                <div className="text-right">
+                    <MDBBtn
+                        color="indigo"
+                        className="m-0"
+                        onClick={userCheckout}
+                        disabled={loading || !image || !selectedBank || !transferDate}
+                    >
+                        {
+                            loading
+                                ? 'Processing . . .'
+                                : 'Checkout'
+                        }
+                    </MDBBtn>
+                </div>
             </MDBModalBody>
 
         </MDBModal>

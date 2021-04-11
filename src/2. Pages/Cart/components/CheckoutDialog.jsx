8462aecd6c57f0b31a700613ad8a2591258b785b
Andre Putera Pratama <andreputerap@gmail.com>
2021-04-11 20:11:18 +0700

    Update CheckoutDialog.jsx

--- src/2. Pages/Cart/components/CheckoutDialog.jsx
+++ src/2. Pages/Cart/components/CheckoutDialog.jsx
@@ -7,19 +7,32 @@ import {
 } from 'mdbreact';
 import APIRequest from '../../../4. Api/APIRequest';
 
+// Images
+import BCA from '../../../3. Assets/img/bank/bca.png';
+import Mandiri from '../../../3. Assets/img/bank/mandiri.png';
+
+// Filepond
+import { FilePond, registerPlugin } from 'react-filepond'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+registerPlugin(FilePondPluginImagePreview)
+
+// Static
 const rekening = [
     {
         bank: 'BCA',
         name: 'Evera Shop',
         number: '3529090123',
+        image: BCA,
     },
     {
         bank: 'Mandiri',
         name: 'Evera Shop',
         number: '112121212',
+        image: Mandiri,
     },
 ]
 
+// Main Component
 const CheckoutDialog = (props) => {
     // PROPS
     const { isOpen, toggleCheckout } = props
@@ -35,8 +48,8 @@ const CheckoutDialog = (props) => {
         try {
             setLoading(true)
 
-            let formData = new FormData
-            formData.append('image', image)
+            let formData = new FormData()
+            formData.append('image', image[0].file)
             formData.append('bank', selectedBank.bank)
             formData.append('nama_rekening', selectedBank.name)
             formData.append('tgl_transfer', transferDate)
@@ -94,6 +107,26 @@ const CheckoutDialog = (props) => {
                             </option>
                         ))}
                     </select>
+                    {selectedBank && (
+                        <div className="row my-3">
+                            <div className="col-5">
+                                <img
+                                    src={selectedBank.image}
+                                    alt={selectedBank.bank}
+                                    className="img-fluid"
+                                />
+                            </div>
+                            <div className="col-7">
+                                <strong>
+                                    {selectedBank.name}
+                                </strong>
+                                <br/>
+                                <span>
+                                    {selectedBank.number}
+                                </span>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
                 <div className="my-3">
@@ -107,16 +140,15 @@ const CheckoutDialog = (props) => {
                 </div>
 
                 <div className="my-3">
-                    <span>Upload</span>
-                    <input
-                        type="file"
-                        accept="image/*"
-                        className="form-control"
-                        onChange={e => setImage(e.target.files[0])}
+                    <span>Transfer Receipt</span>
+                    <FilePond
+                        files={image}
+                        onupdatefiles={setImage}
+                        labelIdle='Drag & Drop or <span class="filepond--label-action">Browse</span>'
                     />
                 </div>
 
-                <div className="text-right">
+                <div className="text-right mt-4">
                     <MDBBtn
                         color="indigo"
                         className="m-0"
